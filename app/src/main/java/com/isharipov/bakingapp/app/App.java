206bb88d511dab8f9a428db56152057272f631ce
ilias <iliamsharipov@gmail.com>
2018-05-24 07:08:14 +0300

    leakcanary added glide added

--- app/src/main/java/com/isharipov/bakingapp/app/App.java
+++ app/src/main/java/com/isharipov/bakingapp/app/App.java
@@ -1,8 +1,10 @@
 package com.isharipov.bakingapp.app;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
 
 import com.isharipov.bakingapp.BuildConfig;
+import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
 
@@ -14,8 +16,27 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initLogger();
+        initLeakCanary();
+    }
+
+    private void initLogger() {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new Timber.Tree() {
+                @Override
+                protected void log(int priority, String tag, @NonNull String message, Throwable t) {
+                    //TODO  decide what to log in release version
+                }
+            });
         }
+    }
+
+    private void initLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
     }
 }
